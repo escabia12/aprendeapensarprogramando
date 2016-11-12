d5030f08dbb432e49b8ff4e372da855cc093e9b6
Rubén Escabia <escabia12@gmail.com>
2016-11-12 13:18:01 +0100

    Calculadora basica

---
+++ calculadoraEntrada5/calculadora_basica.py
@@ -0,0 +1,23 @@
+__author__ = 'Escabia'
+
+''' Calculadora simple que realiza operaciones con dos operandos por entrada y un operador básico (+,-,*,/)'''
+
+operando1 = int(input("Introduzca primer operando: "))
+operador = input("Operador: ")
+operando2 = int(input("Segundo operando: "))
+
+if operador == '+':
+    print (operando1 + operando2)
+
+elif operador == '-':
+    print (operando1 - operando2)
+
+elif operador == '*':
+    print (operando1 * operando2)
+
+elif operador == '/':
+    print (operando1 / operando2)
+else:
+   print("Invalid input")
+
+
